95b90d821e085ee70aa4bd34baa90e2ef46cf353
y73n0k <konal2004@gmail.com>
2022-04-26 14:40:20 +0500

    Remove print

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ class PostInstall(install):
     def run(self):
         super().run()
         module_path = os.path.join(self.install_lib, "cryptosploit_modules")
-        os.system(f"find {module_path} -name do_install.sh -print0 | xargs -0 -I [] sh -c 'cd $(dirname []) && ./do_install.sh'")
-        print(module_path)
+        os.system(f"find {module_path} -name do_install.sh -print0 | xargs -0 -I [] sh -c 'cd $(dirname []) && source ./do_install.sh'")
 
 setup(cmdclass={"install": PostInstall})
