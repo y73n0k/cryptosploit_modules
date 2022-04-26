68c4ee16f337d9859ddaa5f1ae9176f1c64cc02e
SNESEAR <aer2201@bk.ru>
2022-04-26 23:01:29 +0500

    Add templates/ for modules

---
+++ templates/ExampleBinaryModuleName/__init__.py
@@ -0,0 +1,38 @@
+"""
+Template for other programming languages.
+To check full module directory structure (config.json, do_install.sh)
+visit our github https://github.com/y73n0k/cryptosploit_modules
+"""
+
+from cryptosploit.cprint import Printer
+from cryptosploit_modules import BaseModule
+
+class ExampleBinaryModuleName(BaseModule):
+    def __init__(self):
+        super().__init__()
+        self.env.check_var = self.check_var
+
+    @staticmethod
+    def check_var(name, value):
+        """Must return isvalid_variable: bool, error_msg: str"""
+        match name:
+            case "key":
+                if value.isdigit():
+                    return True, ""
+                return False, "Must be a digit"
+            case _:
+                return True, ""
+
+    def run(self):
+        """
+        A function that is called when the user
+        uses the run command
+        """
+        key = self.env.get_var("key").value
+        ciphertext = self.env.get_var("ciphertext").value
+        if key and ciphertext:
+            return self.command_exec(f"your_binary -k {key} -ct {ciphertext}")
+        return Printer.error("All paramaters must be set")
+
+
+module = ExampleBinaryModuleName()
---
+++ templates/__init__.py
@@ -0,0 +1,15 @@
+"""
+Templates for cryptosploit modules.
+
+You can use one of the suggested templates depending on the type of your module.
+
+    Module written in python
+    Module written in another programming language
+
+Warning!
+
+At the end of the __init.py__ file you must create class instance with name 'module'.
+
+To check full module directory structure (config.json, do_install.sh)
+visit our github https://github.com/y73n0k/cryptosploit_modules
+"""
