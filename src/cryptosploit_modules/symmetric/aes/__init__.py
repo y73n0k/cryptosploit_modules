9c305dc2cfbe363359a758b4eadb5df9e47538a9
y73n0k <konal2004@gmail.com>
2022-05-02 22:41:01 +0500

    Add aes

--- src/cryptosploit_modules/symmetric/aes/__init__.py
+++ src/cryptosploit_modules/symmetric/aes/__init__.py
@@ -1,4 +1,5 @@
-from base64 import b64encode
+from base64 import b64encode, b64decode
+from binascii import Error
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 from Crypto.Random import get_random_bytes
@@ -13,14 +14,23 @@ class AESModule(BaseModule):
         self.env.check_var = self.check_var
 
     def check_var(self, name, value):
+        def is_valid_base64(value):
+            try:
+                return b64decode(value)
+            except Error:
+                return False
         match name:
             case "aes_mode":
                 if value not in ["ECB", "CBC", "OFB"]:
                     return False, "Mode must be ECB/CBC/OFB"
             case "iv":
+                if not (value := is_valid_base64(value)):
+                    return False, "Value of initialization vector is not a valid base64"
                 if len(value) != 16:
                     return False, "Initialization vector must be 16 bytes"
             case "key":
+                if not (value := is_valid_base64(value)):
+                    return False, "Value of key is not a valid base64"
                 if len(value) * 8 not in [128, 192, 256]:
                     return False, "Key length must be 128/192/256 bits"
             case "mode":
@@ -29,16 +39,19 @@ class AESModule(BaseModule):
         return True, ""
 
     def run(self):
-        key = self.env.get_var("key").value.encode()
+        key = self.env.get_var("key").value
         if not key:
             key = get_random_bytes(32)
+        else:
+            key = b64decode(key)
         aes_mode = self.env.get_var("aes_mode").value
         res = {"key": b64encode(key).decode()}
         if aes_mode == "ECB":
             cipher = AES.new(key, getattr(AES, f"MODE_{aes_mode}"))
         else:
-            iv = self.env.get_var("iv").value.encode()
+            iv = self.env.get_var("iv").value
             if iv:
+                iv = b64decode(iv)
                 cipher = AES.new(key, getattr(AES, f"MODE_{aes_mode}"), iv=iv)
             else:
                 cipher = AES.new(key, getattr(AES, f"MODE_{aes_mode}"))
@@ -46,12 +59,11 @@ class AESModule(BaseModule):
             res["iv"] = b64encode(iv).decode()
         if self.env.get_var("mode").value == "encrypt":
             res["res"] = b64encode(
-                cipher.encrypt(pad(self.env.get_var("plaintext").value.encode(), AES.block_size))
+                cipher.encrypt(pad(b64decode(self.env.get_var("plaintext").value), AES.block_size))
             ).decode()
         else:
             res["res"] = b64encode(
-                unpad(cipher.decrypt(self.env.get_var("plaintext").value.encode())),
-                AES.block_size,
+                unpad(cipher.decrypt(b64decode(self.env.get_var("plaintext").value)), AES.block_size)
             ).decode()
         Printer.positive(str(res))
 
