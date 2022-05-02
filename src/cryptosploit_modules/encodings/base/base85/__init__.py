4fd0e213f1a1b447eef2d87be769cd68adc4fbc4
SNESEAR <aer2201@bk.ru>
2022-05-03 00:01:08 +0500

    Encodings init

---
+++ src/cryptosploit_modules/encodings/base/base85/__init__.py
@@ -0,0 +1,35 @@
+from cryptosploit.cprint import Printer
+from cryptosploit_modules import BaseModule
+
+class Base85(BaseModule):
+    def __init__(self):
+        super().__init__()
+        self.env.check_var = self.check_var
+
+    @staticmethod
+    def check_var(name, value):
+        """Must return isvalid_variable: bool, error_msg: str"""
+        match name:
+            case "key":
+                if value.isdigit():
+                    return True, ""
+                return False, "Must be a digit"
+            case _:
+                return True, ""
+
+    def encode_command(self):
+       """Encrypt function"""
+
+    def decode_command(self):
+       """Decrypt function"""
+
+    def run(self):
+        """
+        A function that is called when the user
+        uses the run command
+        """
+        func = getattr(self, self.env.get_var("mode").value + "_command")
+        return func()
+
+
+module = Base85()
