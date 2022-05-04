1f338c5da7c3124cd25c136fa1aec61de0d640e5
SNESEAR <aer2201@bk.ru>
2022-05-04 14:55:21 +0500

    Add hex encoding

--- src/cryptosploit_modules/encodings/hex/__init__.py
+++ src/cryptosploit_modules/encodings/hex/__init__.py
@@ -1,5 +1,11 @@
+from binascii import Error
+from codecs import encode, decode
+from os.path import isfile
+
 from cryptosploit.cprint import Printer
 from cryptosploit_modules import BaseModule
+from cryptosploit.exceptions import ArgError
+
 
 class Hex(BaseModule):
     def __init__(self):
@@ -10,29 +16,50 @@ class Hex(BaseModule):
     def check_var(name, value):
         """Must return isvalid_variable: bool, error_msg: str"""
         match name:
-            case "key":
-                if value.isdigit():
+            case "mode":
+                if value in ("to_hex", "from_hex"):
                     return True, ""
-                return False, "Must be a digit"
+                return False, "May be to_hex/from_hex"
+            case "input":
+                if len(bytes(value, encoding="utf-8")) == len(value):
+                    return True, ""
+                return (
+                    False,
+                    "Your string must be a utf-8 string or path to the file to be processed",
+                )
             case _:
                 return True, ""
 
-    def encrypt_command(self):
-       """Encrypt function"""
+    def to_hex_command(self, inp, delimiter):
+        if isfile(inp):
+            with open(inp) as f:
+                inp = f.read()
+        inp = bytes(inp, encoding="utf-8")
+        output = encode(inp, "hex").decode("utf-8")
+        output = delimiter + delimiter.join(
+            [output[i : i + 2] for i in range(0, len(output), 2)]
+        )
+        Printer.positive("Encoded string:\n" + output)
 
-    def decrypt_command(self):
-       """Decrypt function"""
-
-    def attack_command(self):
-       """Attack cipher function"""
+    def from_hex_command(self, inp, delimiter):
+        if isfile(inp):
+            with open(inp) as f:
+                inp = f.read()
+        inp = bytes("".join(inp.split(delimiter)), encoding="utf-8")
+        try:
+            output = decode(inp, "hex")
+        except Error as err:
+            raise ArgError("Your input is not valid hex string") from err
+        Printer.positive("Decoded string:\n" + output.decode("utf-8"))
 
     def run(self):
-        """
-        A function that is called when the user
-        uses the run command
-        """
-        func = getattr(self, self.env.get_var("mode").value + "_command")
-        return func()
+        mode = self.env.get_var("mode").value
+        inp = self.env.get_var("input").value
+        delimiter = self.env.get_var("delimiter").value
+        if mode and inp and delimiter:
+            func = getattr(self, mode + "_command")
+            return func(inp, delimiter)
+        raise ArgError("All variables must be set")
 
 
 module = Hex
