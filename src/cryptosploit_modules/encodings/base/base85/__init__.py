da47b48ccd767eb57ea89aafb9c7e08b96448cac
SNESEAR <aer2201@bk.ru>
2022-05-06 17:41:04 +0500

    Modify base encodings

--- src/cryptosploit_modules/encodings/base/base85/__init__.py
+++ src/cryptosploit_modules/encodings/base/base85/__init__.py
@@ -15,37 +15,30 @@ class Base85(BaseModule):
     @staticmethod
     def check_var(name, value):
         match name:
-            case "input":
-                if len(bytes(value, encoding="utf-8")) == len(value):
-                    return True, ""
-                return (
-                    False,
-                    "Your string must be a utf-8 string or path to the file to be processed",
-                )
             case "mode":
                 if value in ("encode", "decode"):
                     return True, ""
                 return False, "May be decode/encode"
+            case _:
+                return True, ""
 
     def encode_command(self, inp):
-        if isfile(inp):
-            with open(inp) as f:
-                inp = f.read()
-        text = bytes(inp, encoding="utf-8")
-        Printer.positive("Encoded string:\n" + b85encode(text).decode())
+        text = b85encode(inp.encode()).decode()
+        Printer.positive("Encoded string:\n" + text)
 
     def decode_command(self, inp):
-        if isfile(inp):
-            with open(inp) as f:
-                inp = f.read()
-        text = bytes(inp, encoding="utf-8")
         try:
-            Printer.positive("Decoded string:\n" + b85decode(text).decode())
-        except Error as err:
+            text = b85decode(inp.encode()).decode()
+        except (ValueError, Error) as err:
             raise ArgError("Your input is not valid base85 string") from err
+        else:
+            Printer.positive("Decoded string:\n" + text)
 
     def run(self):
         inp = self.env.get_var("input").value
+        if isfile(inp):
+            with open(inp) as f:
+                inp = f.read()
         if inp:
             func = getattr(self, self.env.get_var("mode").value + "_command")
             return func(inp)
